305bb6b5b56d1eeaa11c67e8d3ada85bceb6de01
Paul <isakura313@gmail.com>
2020-01-30 20:30:04 +0300

    asdas

--- todolist/models.py
+++ todolist/models.py
@@ -1,32 +1,21 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
-from django.utils import timezone
-# в наших делах можно будет посмотреть время
-
-
-class Category(models.Model): #таблица категорий имя которая наследует models.Model
-    name = models.CharField(max_length = 100)  #создаем первую категорию varchar
-
+class Category(models.Model): # The Category table name that inherits models.Model
+    name = models.CharField(max_length=100) #Like a varchar
     class Meta:
         verbose_name = ("Category")
         verbose_name_plural = ("Categories")
     def __str__(self):
-        return self.name #имя которое показывается когда она вызывается
-
-class TodoList(models.Model):
-    title = models.CharField(max_length=250) #таблица
-    content = models.TextField(blank=True)  # сам контент нашей todo
-    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))  # a date
-    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))  # a date
-    category = models.ForeignKey(Category, default="general", on_delete=models.PROTECT)  # a foreignkey
-
+        return self.name #name to be shown when called
+class TodoList(models.Model): #Todolist able name that inherits models.Model
+    title = models.CharField(max_length=250) # a varchar
+    content = models.TextField(blank=True) # a text field
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+    id_timestamp = models.CharField(max_length=50)
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+    category = models.ForeignKey(Category, default="general",on_delete=models.PROTECT) # a foreignkey
     class Meta:
-        ordering = ["-created"]  # ordering by the created field
-
+        ordering = ["-created"] #ordering by the created field
     def __str__(self):
-        return self.title  # name to be shown when called
-
-
-
-
+        return self.title #name to be shown when called
--- todolist/views.py
+++ todolist/views.py
@@ -6,15 +6,22 @@ def index(request): #the index view
     if request.method == "POST": #checking if the request method is a POST
         if "taskAdd" in request.POST: #checking if there is a request to add a todo
             title = request.POST["description"] #title
+            # id_todo = request.POST["created"].strftime("%s")
             date = str(request.POST["date"]) #date
             category = request.POST["category_select"] #category
             content = title + " -- " + date + " " + category #content
-            Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
+            Todo = TodoList(title=title, content=content,  due_date=date, category=Category.objects.get(name=category))
             Todo.save() #saving the todo
             return redirect("/") #reloading the page
         if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-            checkedlist = request.POST["checkedbox"] #checked todos to be deleted
-            for todo_id in checkedlist:
-                todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
-                todo.delete() #deleting todo
+            checkedlist = request.POST.getlist('checkedbox') #checked todos to be deleted
+            print(checkedlist)
+            for i in range(len(checkedlist)):
+                try:
+                    print(int(checkedlist[i]))
+                    todo = TodoList.objects.filter(id=int(checkedlist[i]))
+                    todo.delete() #deleting todo
+                except BaseException:
+                    print("я не могу удалить")
+                    # print(TodoList.objects.get(id=20))
     return render(request, "index.html", {"todos": todos, "categories":categories})
