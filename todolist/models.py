b505d7d48ed7a6dc0a0913e3612e525189e90561
Paul <isakura313@gmail.com>
2020-01-31 14:38:58 +0300

    commit asdf

--- todolist/models.py
+++ todolist/models.py
@@ -2,20 +2,19 @@ from django.utils import timezone
 from django.db import models
 
 class Category(models.Model): # The Category table name that inherits models.Model
-    name = models.CharField(max_length=100) #Like a varchar
+    name = models.CharField(max_length=100) #varchar
     class Meta:
         verbose_name = ("Category")
         verbose_name_plural = ("Categories")
     def __str__(self):
-        return self.name #name to be shown when called
-class TodoList(models.Model): #Todolist able name that inherits models.Model
-    title = models.CharField(max_length=250) # a varchar
-    content = models.TextField(blank=True) # a text field
-    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
-    id_timestamp = models.CharField(max_length=50)
-    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
-    category = models.ForeignKey(Category, default="general",on_delete=models.PROTECT) # a foreignkey
+        return self.name #имя которое показывается когда вызывается методд
+class TodoList(models.Model): #Todolist наследует models.Model
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True) #текстовое поле
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # дата создания
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) #  до какой даты нужно было сделать дело
+    category = models.ForeignKey(Category, default="general",on_delete=models.PROTECT) # a foreignkey с помощью которой мы будем осуществлять связь с таблицей Категорий
     class Meta:
-        ordering = ["-created"] #сортировка по их созданию
+        ordering = ["-created"] #сортировка дел по их созданию
     def __str__(self):
-        return self.title #name to be shown when called
+        return self.title
--- todolist/views.py
+++ todolist/views.py
@@ -1,49 +1,38 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect #для отобрежения и редиректа берем необходимые классы
+from django.contrib import messages #импорт сообщений на будущее, если пользователь вдруг захочет
 from .models import TodoList, Category
 def todo(request): #the index view
-    todos = TodoList.objects.all() #quering all todos with the object manager
-    categories = Category.objects.all() #getting all categories with object manager
-    if request.method == "POST": #checking if the request method is a POST
-        if "taskAdd" in request.POST: #checking if there is a request to add a todo
-            title = request.POST["description"] #title
-            # id_todo = request.POST["created"].strftime("%s")
-            date = str(request.POST["date"]) #date
-            category = request.POST["category_select"] #category
-            content = title + " -- " + date + " " + category #content
+    todos = TodoList.objects.all() #запрашиваем все объекты todo через менеджер объектов
+    categories = Category.objects.all() #так же получаем все Категории
+    if request.method == "POST": #проверяем то что метод POST
+        if "taskAdd" in request.POST: #проверяем метод добавления todo
+            title = request.POST["description"] #сам текст
+            date = str(request.POST["date"]) #дата, до которой должно быть закончено дело
+            category = request.POST["category_select"] #категория, которой может выбрать или создать пользователь. По умолчанию ему будет доступно работа и досуг
+            content = title + " -- " + date + " " + category # полный склееный контент
             Todo = TodoList(title=title, content=content,  due_date=date, category=Category.objects.get(name=category))
-            Todo.save() #saving the todo
-            return redirect("/todo") #reloading the page
-        if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-            checkedlist = request.POST.getlist('checkedbox') #checked todos to be deleted
-            print(checkedlist)
+            Todo.save() # сохранение нашего дела
+            return redirect("/todo") # перегрузка страницы ( ну вот так у нас будет устроено очищение формы)
+        if "taskDelete" in request.POST: #если пользователь собирается удалить одно дело
+            checkedlist = request.POST.getlist('checkedbox') # берем список выделенные дел, которые мы собираемся удалить
             for i in range(len(checkedlist)):
-                try:
-                    print(int(checkedlist[i]))
                     todo = TodoList.objects.filter(id=int(checkedlist[i]))
-                    todo.delete() #deleting todo
-                except BaseException:
-                    print("я не могу удалить")
-                    # print(TodoList.objects.get(id=20))
-    return render(request, "todo.html", {"todos": todos, "categories":categories})
+                    todo.delete() #удаление дела
+    return render(request, "todo.html", {"todos": todos, "categories": categories})
 
 
 def category(request):
-    categories = Category.objects.all()  # getting all categories with object manager
-    if request.method == "POST": #checking if the request method is a POST
-        if "taskAdd" in request.POST: #checking if there is a request to add a todo
+    categories = Category.objects.all()  #запрашиваем все объекты Категорий
+    if request.method == "POST": #проверяем что это метод POST
+        if "taskAdd" in request.POST: #если собираемся добавить
             name = request.POST["name"] #имя нашей категории
-            category = Category(name=name)
-            category.save() #saving the todo
+            category = Category(name=name) #у нашей категории есть только имя
+            category.save() # сохранение нашей категории
             return redirect("/category") #reloading the page
-        if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-            cl = request.POST.getlist('check') #checked category to be deleted
-            print(len(cl))
-            for i in range(len(cl)):
-                try:
-                    print(int(cl[i]))
-                    Categ = Category.objects.filter(id=int(cl[i]))
-                    Categ.delete() #deleting todo
-                except BaseException:
-                    print("я не могу удалить")
-                    # print(TodoList.objects.get(id=20))
+        if "taskDelete" in request.POST: # проверяем есть ли удаление
+            check = request.POST.getlist('check') #немного изменил в отличии от todo, что бы было меньше путаницы
+            for i in range(len(check)):
+                print(int(check[i]))
+                сateg = Category.objects.filter(id=int(check[i]))
+                сateg.delete()   #удаление категории
     return render(request, "category.html", {"categories": categories})
